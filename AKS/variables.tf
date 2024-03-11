49521cd2ecd2db9c71c8b0cd7606f69414eda62a
dodlaraghram <113520166+dodlaraghram@users.noreply.github.com>
2024-03-11 21:08:40 +0530

    Update variables.tf

--- AKS/variables.tf
+++ AKS/variables.tf
@@ -1,13 +1,13 @@
-/*
+
 variable "resource_group_location" {
   type        = string
-  default     = "eastus"
+  default     = "southindia"
   description = "Location of the resource group."
 }
 
 variable "resource_group_name_prefix" {
   type        = string
-  default     = "rg"
+  default     = "demorg"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
@@ -28,4 +28,3 @@ variable "username" {
   description = "The admin username for the new cluster."
   default     = "azureadmin"
 }
-*/
