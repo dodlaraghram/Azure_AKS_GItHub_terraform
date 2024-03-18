7da3b73e43908b27c3709a65ca9fe14487d8a385
dodlaraghram <113520166+dodlaraghram@users.noreply.github.com>
2024-03-18 21:02:06 +0530

    Update providers.tf

--- AKS/providers.tf
+++ AKS/providers.tf
@@ -24,7 +24,7 @@ terraform {
       storage_account_name = "azstoreaccdemotest"
       container_name       = "aztffilestore"
       key                  = "terraform.tfstate"
-      use_oidc             = true
+     # use_oidc             = true
       subscription_id      = "ae231b2f-8211-4f88-b5c1-df0bf3fd97f0"
        tenant_id           = "ae58859f-9c48-4516-b237-63f152186d8d"
       client_id            = "091fd078-3ba1-4aba-82f9-4cf3b28038a2"
@@ -32,6 +32,6 @@ terraform {
 }
 
 provider "azurerm" {
-  use_oidc = true
+#  use_oidc = true
   features {}
 }
